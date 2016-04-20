1f88b294fa34fc0682688fa5956cddc36181b2f5
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-04-20 01:05:35 -0700

    Removed unused bindings

--- scripts/components/AdminCenterForm.js
+++ scripts/components/AdminCenterForm.js
@@ -5,81 +5,89 @@ import '../../sass/index.scss';
 
 export default class AdminCenterForm extends Component {
 
-	constructor(props) {
-    	super(props);
-  		this.handleMessageChanged = this.handleMessageChanged.bind(this);
-    	this.sendMessage = this.sendMessage.bind(this);
-    	bindAll(this, [
-  			'handleGroupChange',
-  			'handleCategoryChange',
-  			'handleMessageChanged',
-  			'sendMessage'
-  		]);
-  	};
+  constructor(props) {
+    super(props);
+    bindAll(this, [
+      'handleGroupChange',
+      'handleCategoryChange',
+      'handleMessageChanged',
+      'sendMessage'
+    ]);
+  };
 
-	sendMessage(e) {
-  		e.preventDefault();
-  		this.setState({message: ""});
+  sendMessage(e) {
+    e.preventDefault();
+    this.setState({message: ""});
 
-  		// << to be replaced by the redux code >>
-  	};
+    // << to be replaced by the redux code >>
+  };
 
-  	handleGroupChange(selectedGroup) {
-  		this.setState({selectedGroup});
-  	};
+  handleGroupChange(selectedGroup) {
+    this.setState({selectedGroup});
+  };
 
-  	handleCategoryChange(selectedCategory) {
-  		this.setState({selectedCategory});
-  	};
+  handleCategoryChange(selectedCategory) {
+    this.setState({selectedCategory});
+  };
 
-  	handleMessageChanged(e) {
-  		this.setState({message: e.target.value});
-  	}
+  handleMessageChanged(e) {
+    this.setState({message: e.target.value});
+  }
 
-  	loadGroups() {
+  loadGroups() {
 
-  		// << to be replaced by the actual redux call >>
-  		return [{"name": "HR Group","description": " This is a HR group."},
-  				{"name": "IT Group","description": " This is a IT group."}];
-  	};
+    // << to be replaced by the actual redux call >>
+    return [{"name": "HR Group", "description": " This is a HR group."},
+      {"name": "IT Group", "description": " This is a IT group."}];
+  };
 
-  	loadCategories() {
+  loadCategories() {
 
-  		// << to be replaced by the actual redux call >>
-  		return [{"name":"Warning"}, {"name":"Information"}, {"name":"Positive"}];
-  	};
+    // << to be replaced by the actual redux call >>
+    return [{"name": "Warning"}, {"name": "Information"}, {"name": "Positive"}];
+  };
 
-  	componentWillMount() {
-  		this.setState({groups: {options: this.loadGroups()}, categories: {options: this.loadCategories()}});
-  	};
+  componentWillMount() {
+    this.setState({
+      groups: {
+        options: this.loadGroups()
+      },
+      categories: {
+        options: this.loadCategories()
+      }
+    });
+  };
 
-	render() {
-		return (
-         <div className="admin-center">
-            <div className="logged-in">
-               <span className="avatar">&nbsp;</span>
-               <a href="#">Tim Lange <i className="fa fa-chevron-down" aria-hidden="true"></i></a>
-            </div>
-            <div>
-      			<div className="messageFormContainer admin-form box">
-      				<h3 className="sendMessageTitle">Send Notification</h3>
-      				<form className="createMessageForm form-group" onSubmit={this.sendMessage}>
-      		    		<textarea
-      		    			placeholder="Please enter your message here."
-      		    			className="messageInput form-control"
-      		    			onChange={this.handleMessageChanged}
-      		    			value={this.state.message}
-      		    		/>
-      		    		<DropDown name="Group" data={this.state.groups} changeHandler={this.handleGroupChange.bind(this)} />
-      		    		<DropDown name="Category" data={this.state.categories} changeHandler={this.handleCategoryChange.bind(this)}/>
-                     <div className="clearfix">
-                       <a className="btn btn-lg btn-primary pull-right" href="#" role="button">
-                         <i className="fa fa-share" aria-hidden="true"></i>Send</a>
-                     </div>
-      				</form>
-      			</div>
-            </div>
-         </div>
-		);
-	};
+  render() {
+    return (
+      <div className="admin-center">
+        <div className="logged-in">
+          <span className="avatar">&nbsp;</span>
+          <a href="#">Tim Lange <i className="fa fa-chevron-down" aria-hidden="true"></i></a>
+        </div>
+        <div>
+          <div className="messageFormContainer admin-form box">
+            <h3 className="sendMessageTitle">Send Notification</h3>
+            <form className="createMessageForm form-group" onSubmit={this.sendMessage}>
+              <textarea
+                placeholder="Please enter your message here."
+                className="messageInput form-control"
+                onChange={this.handleMessageChanged}
+                value={this.state.message}
+              />
+              <DropDown name="Group" data={this.state.groups} changeHandler={this.handleGroupChange.bind(this)}/>
+              <DropDown name="Category" data={this.state.categories}
+                        changeHandler={this.handleCategoryChange.bind(this)}/>
+              <div className="clearfix">
+                <a className="btn btn-lg btn-primary pull-right" href="#" role="button">
+                  <i className="fa fa-share" aria-hidden="true"></i>
+                  Send
+                </a>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  };
 }
--- scripts/components/DropDown.js
+++ scripts/components/DropDown.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 
 export default class DropDown extends Component {
 
-	constructor(props) {
+  constructor(props) {
     super(props);
     this.state = {};
     this.state.options = this.props.data.options;
@@ -17,24 +17,32 @@ export default class DropDown extends Component {
   render() {
     var i = 0;
     var options = this.state.options.map(function (option) {
-        return React.createElement(
-          'option',
-          {value: option.name, key: i++},
-          option.name
-        );
+      return React.createElement(
+        'option',
+        {
+          value: option.name,
+          key: i++
+        },
+        option.name
+      );
     });
 
     return React.createElement("div", {className: "dropDownContainer"},
-        React.createElement(
-          'label',
-          {className: "dropDownLabel"},
-          this.props.name
-        ),
-        React.createElement(
-          'select',
-          {onChange: this.handleChange, className: "dropDown form-control"},
-          options
-        )
+      React.createElement(
+        'label',
+        {
+          className: "dropDownLabel"
+        },
+        this.props.name
+      ),
+      React.createElement(
+        'select',
+        {
+          onChange: this.handleChange,
+          className: "dropDown form-control"
+        },
+        options
+      )
     );
   };
 }
