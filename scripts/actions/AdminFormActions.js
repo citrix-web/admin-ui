d6b8721f55f018288704c1a6974c08a0f4da910d
Mike Leong <mleong28@gmail.com>
2016-04-21 14:49:58 -0700

    superagent

--- scripts/actions/AdminFormActions.js
+++ scripts/actions/AdminFormActions.js
@@ -1,24 +1,26 @@
 import fetch from 'isomorphic-fetch';
-
+import superagent from 'superagent';
 /**
  * Publish a notification
  */
 export function notifSend(notif) {
   console.log('notif', notif)
   return dispatch => {
-    fetch(' http://localhost:3000/queue', {
-      method: 'put',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        message: notif.message,
-        group: notif.group,
-        category: notif.category
-      })
-    })
-      .then(response => {
+    // fetch(' http://localhost:3000/queue', {
+    //   method: 'put',
+    //   headers: {
+    //     'Accept': 'application/json',
+    //     'Content-Type': 'application/json',
+    //   },
+    //   body: JSON.stringify({
+    //     message: notif.message,
+    //     group: notif.group,
+    //     category: notif.category
+    //   })
+    // })
+    superagent.put('http://localhost:3000/queue')
+      .send({ message: notif.message, group: notif.group, category: notif.category })
+      .end((err,response) => {
         if (response.status >= 200 && response.status < 300) {
           console.log(response);
           //dispatch(sendSuccess(response));
@@ -29,8 +31,5 @@ export function notifSend(notif) {
           throw error;
         }
       })
-      .catch(error => {
-        console.log('request failed', error);
-      });
   };
 }
